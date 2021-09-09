6191e4f62198adb89c77a76dfefda5842a99242d
Tweener <vivouneuh@gmail.com>
2021-09-09 12:49:01 +0200

    Improves ActionMessageBottomSheetDialogFragment configuration to show/hide the handle.

--- library/src/main/java/com/spendesk/grapes/ActionMessageBottomSheetDialogFragment.kt
+++ library/src/main/java/com/spendesk/grapes/ActionMessageBottomSheetDialogFragment.kt
@@ -10,6 +10,7 @@ import androidx.annotation.DrawableRes
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.spendesk.grapes.extensions.visibleOrGone
 import com.spendesk.grapes.extensions.visibleWithTextOrGone
 import kotlinx.android.synthetic.main.fragment_bottom_sheet_info.*
 
@@ -28,7 +29,8 @@ open class ActionMessageBottomSheetDialogFragment : BottomSheetDialogFragment() 
         val title: CharSequence,
         val description: CharSequence? = null,
         val primaryButtonText: CharSequence? = null,
-        val secondaryButtonText: CharSequence? = null
+        val secondaryButtonText: CharSequence? = null,
+        val shouldShowHandle: Boolean = true
     )
 
     protected var onPrimaryButtonClicked: (() -> Unit)? = null
@@ -73,14 +75,7 @@ open class ActionMessageBottomSheetDialogFragment : BottomSheetDialogFragment() 
         actionMessageBottomSheetDescriptionText.visibleWithTextOrGone(configuration.description)
         actionMessageBottomSheetPrimaryButton.visibleWithTextOrGone(configuration.primaryButtonText)
         actionMessageBottomSheetSecondaryButton.visibleWithTextOrGone(configuration.secondaryButtonText)
-    }
-
-    fun setPrimaryButtonEnabled(isEnabled: Boolean) {
-        actionMessageBottomSheetPrimaryButton.isEnabled = isEnabled
-    }
-
-    fun setSecondaryButtonEnabled(isEnabled: Boolean) {
-        actionMessageBottomSheetSecondaryButton.isEnabled = isEnabled
+        actionMessageBottomSheetPullView.visibleOrGone(configuration.shouldShowHandle)
     }
 
     private fun bindView() {
