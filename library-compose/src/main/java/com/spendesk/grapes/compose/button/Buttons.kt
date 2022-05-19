bcd51eb8d448462e4e062b21e48e000738feb2b3
Dany Bouca Nova <dany.boucanova@gmail.com>
2022-05-19 11:42:48 +0200

    [Compose] - feature/AA-1749-add-jetpack-compose
    - Simplify button

--- library-compose/src/main/java/com/spendesk/grapes/compose/button/Buttons.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/button/Buttons.kt
@@ -3,9 +3,6 @@ package com.spendesk.grapes.compose.button
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.spendesk.grapes.compose.theme.GrapesTheme
@@ -15,21 +12,12 @@ import com.spendesk.grapes.compose.theme.GrapesTheme
  * @since : 16/05/2022, Mon
  **/
 
-enum class Style {
-    PRIMARY,
-    SECONDARY,
-    WARNING,
-    ALERT
-}
-
 @Composable
 fun GrapesButton(
     modifier: Modifier = Modifier,
     onClick: (() -> Unit)? = null,
-    styleState: State<Style> = mutableStateOf(Style.PRIMARY),
     text: CharSequence
 ) {
-    val style = remember { styleState }
     Text(
         text = text.toString(),
         color = GrapesTheme.colors.mainWhite,
