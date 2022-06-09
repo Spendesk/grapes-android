23e8c6944ccaee9b23eea02d766db2de6364fad8
Dany Bouca Nova <dany.boucanova@gmail.com>
2022-06-09 08:50:02 +0200

    [Compose] - feature/AA-1749-add-jetpack-compose (#102)
    * [Compose] - feature/AA-1749-add-jetpack-compose
    
    - Add compose necessary integrations and theming
    
    * [Compose] - feature/AA-1749-add-jetpack-compose
    
    - Clean
    
    * [Compose] - feature/AA-1749-add-jetpack-compose
    
    - Review clean and fixes
    
    * [Compose] - feature/AA-1749-add-jetpack-compose
    
    - Simplify button
    
    * [Compose] - feature/AA-1749-add-jetpack-compose
    
    - Renaming
    
    * [Jetpack Compose] - feature/AA-1749-add-jetpack-compose
    
    * [Fix] - feature/AA-1749-add-jetpack-compose

---
+++ library-compose/src/main/java/com/spendesk/grapes/compose/theme/Dimensions.kt
@@ -0,0 +1,21 @@
+package com.spendesk.grapes.compose.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * @author : danyboucanova
+ * @since : 16/05/2022, Mon
+ **/
+
+@Immutable
+data class GrapesDimensions(
+    val paddingSmall: Dp = 4.dp,
+    val paddingMedium: Dp = 8.dp,
+    val paddingNormal: Dp = 16.dp,
+    val paddingLarge: Dp = 24.dp
+)
+
+internal val LocalGrapesDimensions = staticCompositionLocalOf { GrapesDimensions() }
---
+++ library-compose/src/main/java/com/spendesk/grapes/compose/theme/Typography.kt
@@ -0,0 +1,102 @@
+package com.spendesk.grapes.compose.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.spendesk.grapes.compose.R
+
+/**
+ * @author : danyboucanova
+ * @since : 03/05/2022, Tue
+ **/
+
+private val DefaultGrapesFontFamily = FontFamily(
+    Font(R.font.font_raw_gt_america_regular, FontWeight.Normal),
+    Font(R.font.font_raw_gt_america_medium, FontWeight.Medium)
+)
+
+@Immutable
+data class GrapesTypography constructor(
+    val bodyXs: TextStyle,
+    val bodyS: TextStyle,
+    val bodyRegular: TextStyle,
+    val bodyM: TextStyle,
+    val bodyL: TextStyle,
+    val bodyXl: TextStyle,
+    val bodyXxl: TextStyle,
+    val titleS: TextStyle,
+    val titleM: TextStyle,
+    val titleL: TextStyle,
+    val titleXl: TextStyle,
+) {
+    constructor(
+        defaultFontFamily: FontFamily = DefaultGrapesFontFamily,
+        BodyXs: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 12.sp,
+        ),
+        BodyS: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 14.sp,
+        ),
+        BodyRegular: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 16.sp,
+        ),
+        BodyM: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 24.sp,
+        ),
+        BodyL: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 32.sp,
+        ),
+        BodyXl: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 40.sp,
+        ),
+        BodyXxl: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 56.sp,
+        ),
+        TitleS: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 14.sp,
+        ),
+        TitleM: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 16.sp,
+        ),
+        TitleL: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 18.sp,
+        ),
+        TitleXl: TextStyle = TextStyle(
+            fontWeight = FontWeight.W300,
+            fontSize = 32.sp,
+        ),
+    ) : this(
+        BodyXs.withDefaultFontFamily(defaultFontFamily),
+        BodyS.withDefaultFontFamily(defaultFontFamily),
+        BodyRegular.withDefaultFontFamily(defaultFontFamily),
+        BodyM.withDefaultFontFamily(defaultFontFamily),
+        BodyL.withDefaultFontFamily(defaultFontFamily),
+        BodyXl.withDefaultFontFamily(defaultFontFamily),
+        BodyXxl.withDefaultFontFamily(defaultFontFamily),
+        TitleS.withDefaultFontFamily(defaultFontFamily),
+        TitleM.withDefaultFontFamily(defaultFontFamily),
+        TitleL.withDefaultFontFamily(defaultFontFamily),
+        TitleXl.withDefaultFontFamily(defaultFontFamily),
+    )
+}
+
+internal val LocalGrapesTypography = staticCompositionLocalOf { GrapesTypography() }
+
+
+private fun TextStyle.withDefaultFontFamily(default: FontFamily): TextStyle {
+    return if (fontFamily != null) this else copy(fontFamily = default)
+}
