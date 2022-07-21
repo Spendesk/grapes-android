3503787fe38dd9cf65af0d26788870a5a58bbcb2
Kelian CLERC <kelian.clerc@spendesk.com>
2022-07-21 12:05:38 +0200

    Create WindowEditText skeleton

---
+++ library-compose/src/main/java/com/spendesk/grapes/compose/edittext/windowed/WindowEditText.kt
@@ -0,0 +1,107 @@
+package com.spendesk.grapes.compose.edittext.windowed
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.spendesk.grapes.compose.theme.GrapesTheme
+
+/**
+ * @author Kélian CLERC
+ * @since 04/07/2022
+ */
+
+private const val DefaultMaxLength = 12
+private val ForbiddenCharRegex = "\\D".toRegex()
+
+
+@Composable
+fun WindowEditTextBase(
+    text: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    maxLength: Int = DefaultMaxLength,
+    cursorColor: Color = GrapesTheme.colors.mainWhite,
+    textStyle: TextStyle = GrapesTheme.typography.bodyM.copy(letterSpacing = 3.sp, color = GrapesTheme.colors.mainWhite, fontSize = 18.sp, fontFeatureSettings = "tnum"),
+) {
+
+    val formattedText = sanitizeWindowedEditTextContent(text, maxLength)
+
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        BasicTextField(
+            value = formattedText,
+            onValueChange = { newValue ->
+                onTextChange(sanitizeWindowedEditTextContent(newValue, maxLength))
+            },
+            textStyle = textStyle,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, autoCorrect = false),
+            cursorBrush = SolidColor(cursorColor),
+        )
+    }
+}
+
+private fun sanitizeWindowedEditTextContent(text: String, maxLength: Int): String {
+    return text.replace(ForbiddenCharRegex, "").run {
+        if (this.length > maxLength) {
+            this.substring(0 until maxLength)
+        } else {
+            this
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WindowEditTextPreview() {
+    GrapesTheme {
+        var content by remember { mutableStateOf("") }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(GrapesTheme.colors.mainPrimaryLight),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Try to select this text to paste it in editText", color = GrapesTheme.colors.mainWhite)
+                Spacer(modifier = Modifier.size(4.dp))
+                SelectionContainer {
+                    Text(text = "1234-5678-9101", color = GrapesTheme.colors.mainWhite)
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Text(text = "Text In edit text")
+                Text(text = content)
+            }
+            WindowEditTextBase(
+                text = content, onTextChange = { content = it }
+            )
+        }
+    }
+}
