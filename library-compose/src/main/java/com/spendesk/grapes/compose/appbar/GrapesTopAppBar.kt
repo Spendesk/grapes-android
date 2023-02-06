b5ca268550d6f6e0397f573e4dba4d1ea6acd2b0
Dany Bouca Nova <dany@spendesk.com>
2023-02-06 14:27:34 +0100

    [Clean] - Remove wildcards imports

--- library-compose/src/main/java/com/spendesk/grapes/compose/appbar/GrapesTopAppBar.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/appbar/GrapesTopAppBar.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
