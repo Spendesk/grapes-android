170909b7b837e3bcb2d4e9355e7f6498e1d65b17
Dany Bouca Nova <dany@spendesk.com>
2023-02-22 19:03:22 +0100

    [Compose][Divider] - Change divider defaults and add preview

--- library-compose/src/main/java/com/spendesk/grapes/compose/listitem/Divider.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/listitem/Divider.kt
@@ -1,10 +1,17 @@
 package com.spendesk.grapes.compose.listitem
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.spendesk.grapes.compose.theme.GrapesTheme
 
 /**
  * @author : dany
@@ -17,3 +24,26 @@ fun GrapesDivider(
     thickness: Dp = DividerDefaults.Thickness
 ) =
     Divider(modifier = modifier, color = color, thickness = thickness)
+
+
+@Preview(
+    widthDp = 372,
+    showBackground = true
+)
+@Composable
+fun GrapesSimpleTopAppBarPreview() {
+    GrapesTheme {
+        LazyColumn {
+            items(4) {
+                Text(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    text = "item number $it"
+                )
+
+                GrapesDivider()
+            }
+        }
+    }
+}
--- library-compose/src/main/java/com/spendesk/grapes/compose/listitem/DividerDefaults.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/listitem/DividerDefaults.kt
@@ -3,7 +3,6 @@ package com.spendesk.grapes.compose.listitem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.spendesk.grapes.compose.theme.GrapesTheme
 
 /**
@@ -13,8 +12,8 @@ import com.spendesk.grapes.compose.theme.GrapesTheme
 object DividerDefaults {
 
     /** Default thickness of a divider. */
-    val Thickness: Dp = 1.0.dp
+    val Thickness: Dp @Composable get() = GrapesTheme.dimensions.dividerThickness
 
     /** Default color of a divider. */
-    val color: Color @Composable get() = GrapesTheme.colors.mainNeutralLighter
+    val color: Color @Composable get() = GrapesTheme.colors.mainNeutralLight
 }
