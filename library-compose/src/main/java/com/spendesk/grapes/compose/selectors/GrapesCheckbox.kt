685ac16d3959bb0100833301618bfbdf8c0670f1
Dany Bouca Nova <dany@spendesk.com>
2023-03-06 14:08:54 +0100

    Task/aa 2366 create checkbox (#159)
    * [Compose] - Add BOM compose catalog
    
    - Add Compose BOM
    
    * [Compose][RadioButton] - Add new radio button component
    
    * [Compose][GrapesRadioButtonText] - Add new radio with text component and change a some things in radiobutton
    
    * [Compose][Checkbox] - Create GrapesCheckbox and GrapesCheckboxText
    
    * [Compose][Checkbox] - Change parameter order in checkbox
    
    * [Clean] - Change parameter order on checkboxes and change preview

---
+++ library-compose/src/main/java/com/spendesk/grapes/compose/selectors/GrapesCheckbox.kt
@@ -0,0 +1,69 @@
+package com.spendesk.grapes.compose.selectors
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.spendesk.grapes.compose.theme.GrapesTheme
+
+/**
+ * @author : dany
+ * @since : 23/02/2023, Thu
+ **/
+@Composable
+fun GrapesCheckbox(
+    isChecked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit),
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+) =
+    Checkbox(
+        modifier = modifier,
+        checked = isChecked,
+        enabled = isEnabled,
+        colors = CheckboxDefaults.colors(
+            checkedColor = GrapesCheckboxDefaultColors.checkedColor,
+            uncheckedColor = GrapesCheckboxDefaultColors.uncheckedColor,
+            checkmarkColor = GrapesCheckboxDefaultColors.checkmarkColor,
+            disabledCheckedColor = GrapesCheckboxDefaultColors.disabledCheckedColor,
+            disabledUncheckedColor = GrapesCheckboxDefaultColors.disabledUncheckedColor,
+            disabledIndeterminateColor = GrapesCheckboxDefaultColors.disabledIndeterminateColor
+        ),
+        onCheckedChange = onCheckedChange
+    )
+
+@Composable
+@Preview(
+    widthDp = 422,
+    showBackground = true,
+)
+private fun GrapesCheckboxPreview() {
+    GrapesTheme {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                GrapesCheckbox(isChecked = true, isEnabled = true, onCheckedChange = { Log.i("GrapesCheckbox", "action checkbox clicked: $it") })
+                Text("Checkbox selected and enabled")
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                GrapesCheckbox(isChecked = false, isEnabled = true, onCheckedChange = { Log.i("GrapesCheckbox", "action checkbox clicked: $it") })
+                Text("Checkbox unselected and enabled")
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                GrapesCheckbox(isChecked = true, isEnabled = false, onCheckedChange = { Log.i("GrapesCheckbox", "action checkbox clicked: $it") })
+                Text("Checkbox selected and disabled")
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                GrapesCheckbox(isChecked = false, isEnabled = false, onCheckedChange = { Log.i("GrapesCheckbox", "action checkbox clicked: $it") })
+                Text("Checkbox unselected and disabled")
+            }
+        }
+    }
+}
