888bc8e72dedca39e4950c03503208be692eded5
Kelian CLERC <kelian.clerc@spendesk.com>
2023-08-26 22:21:31 +0200

    Add showkase module

---
+++ sample/src/main/java/com/spendesk/grapes/samples/home/ShowkaseModule.kt
@@ -0,0 +1,11 @@
+package com.spendesk.grapes.samples.home
+
+import com.airbnb.android.showkase.annotation.ShowkaseRoot
+import com.airbnb.android.showkase.annotation.ShowkaseRootModule
+
+/**
+ * @author Kélian CLERC
+ * @since 25/08/2023
+ */
+@ShowkaseRoot
+class GrapesShowkaseModule: ShowkaseRootModule
