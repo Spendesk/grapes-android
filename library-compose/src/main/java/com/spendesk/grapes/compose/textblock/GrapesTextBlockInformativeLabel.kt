e65ce0d229c7001e3e5bbf6c3b0bcdeda944e886
Kelian CLERC <kelian.clerc@spendesk.com>
2024-01-17 15:07:33 +0100

    Add spacer to `GrapesTextBlockInformativeLabel`

--- library-compose/src/main/java/com/spendesk/grapes/compose/textblock/GrapesTextBlockInformativeLabel.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/textblock/GrapesTextBlockInformativeLabel.kt
@@ -4,24 +4,45 @@ package com.spendesk.grapes.compose.textblock
  * @author Kélian CLERC
  * @since 16/01/2024
  */
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.spendesk.grapes.compose.theme.GrapesTheme
 
 @Composable
-fun GrapesTextBlockInformativeLabel(label: String, color: Color) {
-    Text(
-        text = label,
-        style = GrapesTheme.typography.bodyM,
-        color = color,
-        maxLines = 1,
-    )
+fun GrapesTextBlockInformativeLabel(
+    label: String,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(GrapesTheme.dimensions.spacing2),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(
+            modifier = Modifier
+                .size(4.dp)
+                .background(color, shape = GrapesTheme.shapes.shape4)
+        )
+        Text(
+            text = label,
+            style = GrapesTheme.typography.bodyM,
+            color = color,
+            maxLines = 1,
+        )
+    }
 }
 
 @Preview
@@ -32,8 +53,8 @@ private fun TextBlockOptionalTitlePreview() {
             modifier = Modifier.padding(GrapesTheme.dimensions.paddingLarge),
             verticalArrangement = Arrangement.spacedBy(GrapesTheme.dimensions.paddingLarge)
         ) {
-            GrapesTextBlockInformativeLabel("• Missing", color = GrapesTheme.colors.warningNormal)
-            GrapesTextBlockInformativeLabel("• Optional", color = GrapesTheme.colors.neutralNormal)
+            GrapesTextBlockInformativeLabel("Missing", color = GrapesTheme.colors.warningNormal)
+            GrapesTextBlockInformativeLabel("Optional", color = GrapesTheme.colors.neutralNormal)
         }
     }
 }
