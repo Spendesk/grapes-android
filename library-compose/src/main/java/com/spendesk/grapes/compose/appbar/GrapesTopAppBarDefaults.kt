1c652ed920e1470f5c2890a7cacc2ce54e483fec
Romain Goutte-Fangeas <romain.goutte-fangeas@spendesk.com>
2024-03-05 14:54:19 +0100

    Expose missing attributes of MediumTopAppBar

---
+++ library-compose/src/main/java/com/spendesk/grapes/compose/appbar/GrapesTopAppBarDefaults.kt
@@ -0,0 +1,26 @@
+package com.spendesk.grapes.compose.appbar
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.spendesk.grapes.compose.theme.GrapesTheme
+
+object GrapesTopAppBarDefaults {
+
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun colors(
+        containerColor: Color = GrapesTheme.colors.structureBackground,
+        scrolledContainerColor: Color = GrapesTheme.colors.structureBackground,
+        navigationIconContentColor: Color = GrapesTheme.colors.structureComplementary,
+        titleContentColor: Color = GrapesTheme.colors.structureComplementary,
+        actionIconContentColor: Color = GrapesTheme.colors.structureComplementary,
+    ) = TopAppBarDefaults.mediumTopAppBarColors(
+        containerColor = containerColor,
+        scrolledContainerColor = scrolledContainerColor,
+        navigationIconContentColor = navigationIconContentColor,
+        titleContentColor = titleContentColor,
+        actionIconContentColor = actionIconContentColor,
+    )
+}
