b35fd5d42a12d2ab43dd5afd17e942bd7227fc23
Dany Bouca Nova <dany@spendesk.com>
2024-03-14 15:19:56 +0100

    [GrapesBadge] - Handle animated counter

--- library-compose/src/main/java/com/spendesk/grapes/compose/badge/GrapesBadge.kt
+++ library-compose/src/main/java/com/spendesk/grapes/compose/badge/GrapesBadge.kt
@@ -1,14 +1,26 @@
 package com.spendesk.grapes.compose.badge
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -82,23 +94,63 @@ private fun GrapesBadge(
             )
             .padding(horizontal = GrapesBadgeDefaults.HorizontalPadding)
     ) {
-        val textValue = when {
-            countValue > maxCount -> "$maxCount$moreIndicator"
-            else -> countValue.toString()
+        Row(
+            modifier = Modifier
+                .wrapContentSize()
+                .align(Alignment.Center)
+                .animateContentSize()
+        ) {
+            countValue.toString()
+                .mapIndexed { index, c -> Digit(c, countValue, index) }
+                .forEach { digit ->
+                    AnimatedContent(
+                        modifier = modifier,
+                        targetState = digit,
+                        transitionSpec = {
+                            if (targetState > initialState) {
+                                slideInVertically { -it } togetherWith slideOutVertically { it }
+                            } else {
+                                slideInVertically { it } togetherWith slideOutVertically { -it }
+                            }
+                        },
+                        label = "Animated Badge Content"
+                    ) { animatedDigit ->
+                        val textValue = when {
+                            animatedDigit.fullNumber > maxCount && animatedDigit.place == 0 -> "$moreIndicator$maxCount"
+                            animatedDigit.fullNumber > maxCount && animatedDigit.place > 0 -> ""
+                            else -> animatedDigit.digitChar.toString()
+                        }
+                        Text(
+                            text = textValue,
+                            style = GrapesBadgeDefaults.textTypography(colors.textColor()),
+                        )
+                    }
+                }
         }
-
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            style = GrapesBadgeDefaults.textTypography(colors.textColor()),
-            text = textValue,
-        )
     }
+}
+
+internal data class Digit(val digitChar: Char, val fullNumber: Int, val place: Int) {
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Digit -> digitChar == other.digitChar
+            else -> super.equals(other)
+        }
+    }
+}
+
+internal operator fun Digit.compareTo(other: Digit): Int {
+    return fullNumber.compareTo(other.fullNumber)
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF)
 @Composable
 private fun GrapesBadgesPreview(@PreviewParameter(GrapesBadgeProvider::class) type: GrapesBadgeType) {
     GrapesTheme {
+        var count1 by remember { mutableIntStateOf(12) }
+        var count2 by remember { mutableIntStateOf(95) }
+
         Row(
             modifier = Modifier.padding(GrapesTheme.dimensions.spacing2),
             horizontalArrangement = Arrangement.spacedBy(GrapesTheme.dimensions.spacing2),
@@ -106,9 +158,16 @@ private fun GrapesBadgesPreview(@PreviewParameter(GrapesBadgeProvider::class) ty
         ) {
             when (type) {
                 GrapesBadgeType.NEUTRAL -> {
-                    GrapesNeutralBadge(countValue = 1)
-                    GrapesNeutralBadge(countValue = 100)
+                    GrapesNeutralBadge(countValue = count1)
+                    GrapesNeutralBadge(countValue = count2)
                     GrapesNeutralBadge(countValue = 11, maxCount = 10)
+
+                    Column {
+                        Button(onClick = { count1 += 1 }) { Text("Increment first count") }
+                        Button(onClick = { count1 -= 1 }) { Text("Decrement first count") }
+                        Button(onClick = { count2 += 1 }) { Text("Increment second count") }
+                        Button(onClick = { count2 -= 1 }) { Text("Decrement second count") }
+                    }
                 }
 
                 GrapesBadgeType.PRIMARY -> {
